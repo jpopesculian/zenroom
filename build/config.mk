4309f350081af4543f6866816abb243585d36e87
Daniele Lacamera <root@danielinux.net>
2019-03-06 21:03:25 +0100

    Added linker script, fixed linker flags

--- build/config.mk
+++ build/config.mk
@@ -69,8 +69,9 @@ ld := arm-none-eabi-ld
 system := Generic
 ldadd += -lm
 cflags_protection := ""
-cflags := ${cflags_protection} -DARCH_CORTEX -O3 -Wall -Wextra -pedantic -std=gnu99 -mcpu=cortex-m4 -mthumb -mlittle-endian -mthumb-interwork -Wstack-usage=1024 -DLIBRARY
+cflags := ${cflags_protection} -DARCH_CORTEX -O3 -Wall -Wextra -pedantic -std=gnu99 -mcpu=cortex-m4 -mthumb -mlittle-endian -mthumb-interwork -Wstack-usage=1024 -DLIBRARY -Os -Wno-main -ffreestanding -Wl,-gc-sections -nostartfiles
 milagro_cmake_flags += -DCMAKE_SYSTEM_PROCESSOR="arm" -DCMAKE_CROSSCOMPILING=1 -DCMAKE_C_COMPILER_WORKS=1
+ldflags+=-mcpu=cortex-m4 -mthumb -mlittle-endian -mthumb-interwork -Wstack-usage=1024 -ggdb -Wno-main -ffreestanding -T cortex_m.ld
 endif
 
 
