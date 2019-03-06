35a65e6f590289bf3815d2ef6bc15c47ad31fadb
Jaromil <jaromil@dyne.org>
2019-03-06 20:45:19 +0100

    clean unused headers in jutils

--- src/jutils.c
+++ src/jutils.c
@@ -24,8 +24,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <time.h> // nanosleep
-#include <sys/time.h> // gettimeofday
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
